09aa2e4af82340b3922e631b0b6f8109fe31ddc9
rocean <rocean74@gmail.com>
2019-05-03 16:13:06 +0300

    Init C files

---
+++ main.c
@@ -0,0 +1,13 @@
+/**
+ *
+ * File: main.c
+ *
+ * Created by Yiannis Kiranis <std138327@ac.eap.gr>
+ *
+ * Date: 2019-05-03
+ * Time: 16:12
+ *
+ * Experimental program to create random systems for tzoker
+ *
+ */
+
