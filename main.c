e0e54558b0b94dc245128b2b548255bfba3e0511
rocean <rocean74@gmail.com>
2019-05-03 17:22:26 +0300

    Get users data

--- main.c
+++ main.c
@@ -11,3 +11,68 @@
  *
  */
 
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_NUMBER 45
+#define MAX_TZOKER 20
+#define COLUMN_COST 0.50
+
+typedef struct node {
+    int number;
+    struct node *next;
+} RandomNumbers;
+
+RandomNumbers *randomNumbersList;
+
+/**
+ * Get the data from user
+ *
+ * @param numbers
+ * @param tzokers
+ * @param cost
+ */
+void getUserData(int *numbers, int *tzokers, double *cost)
+{
+    printf("\n");
+    do {
+        printf("Πόσους τυχαίους αριθμούς θέλεις να δημιουργήσεις? ");
+        scanf("%d", numbers);
+
+        if( (*numbers < 1) || (*numbers > MAX_NUMBER) ) {
+            printf("\nΠρέπει να επιλέξεις μεταξύ 1-%d\n", MAX_NUMBER);
+        }
+    } while ( (*numbers < 1) || (*numbers > MAX_NUMBER) );
+
+    printf("\n");
+    do {
+        printf("Πόσους τυχαίους αριθμούς ΤΖΟΚΕΡ θέλεις να δημιουργήσεις? ");
+        scanf("%d", tzokers);
+
+        if( (*tzokers < 1) || (*tzokers > MAX_TZOKER) ) {
+            printf("\nΠρέπει να επιλέξεις μεταξύ 1-%d\n", MAX_TZOKER);
+        }
+    } while ( (*tzokers < 1) || (*tzokers > MAX_TZOKER) );
+
+    printf("\n");
+    do {
+        printf("Ποιο θέλεις να είναι το μέγιστο κόστος? ");
+        scanf("%lf", cost);
+
+        if((*cost < COLUMN_COST)) {
+            printf("\nΤο ποσό πρέπει να είναι τουλάχιστον %3.2lf\n", COLUMN_COST);
+        }
+    } while (*cost < COLUMN_COST);
+}
+
+int main()
+{
+    int numbers, tzokers;
+    double cost;
+
+    getUserData(&numbers, &tzokers, &cost);
+
+    printf("\nUser Data: %d, %d, %3.2lf \n", numbers, tzokers, cost);
+
+    return 0;
+}
